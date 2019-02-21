fa11265c5b2701b38eeaeb43d10ff48cda7d1379
Illia Maroz <morozzz.ilya@gmail.com>
2019-02-21 13:37:25 +0300

    save

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
-const firstResponse = {};
+const  request  = require('async-request');
+const  { JSDOM } = require("jsdom");
+const fs = require('fs');
+
+const CHECK_INTERVAL = 2000; //ms
+const DURATION = 20; //min
+const LIMIT_OF_TRIES = DURATION * 60000 / CHECK_INTERVAL;
+
+const register = (options) => {
+    return request('https://extrareality.by/shturm_minsk', {
+       method: 'POST',
+       ...options 
+    });
+}
+
+const getData = (options) => {
+    return request('https://extrareality.by/shturm_minsk', {
+        method: 'GET',
+        ...options
+    })
+    .catch((err) => {
+        console.log(err);        
+    });
+}
+
+const wait = (timeInMs) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, timeInMs);
+    });
+}
 
 const getForm = () => {
-    fetch('https://extrareality.by/shturm_minsk', {
-        method: 'GET',
-        mode: 'no-cors',
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-        }
-        // headers: {
-        //     'Access-Control-Allow-Origin': '*',
-        //     'Origin': 'https://extrareality.by',
-        //     'Accept-Encoding': 'gzip, deflate, br',
-        //     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        //     'Referer': 'https://extrareality.by/shturm_minsk',
-        //     'X-Requested-With': 'XMLHttpRequest',
-        //     "Content-Type": "text/html"
-        // }
-    })
-        .then(response => {
-            console.log(response);
-            
-            return response.json()
-        })
-        .then(data => {
-            // firstResponse['Cookie'] = data.headers['Set-Cookie'];
-            console.log(data);
-        })
-        .catch(err => {
-            console.log(err);            
-        })
-  
+    let count = 0;
+    let form;
+
+    while (!from && count <= LIMIT_OF_TRIES) {
+        const data = await get
+    }
     
+        getData()
+            .then(res => {
+                count++;
+                console.log(count);
+                
+                if (count > LIMIT_OF_TRIES) {
+                    clearInterval(checkId);
+                    return false;
+                }
+
+                form = new JSDOM(res.body).window.document.getElementById('registration-form');
+                console.log(form, 1);
+                
+                if(form) {
+                    clearInterval(checkId);
+                    return form;
+                } 
+            });
+    }, CHECK_INTERVAL);
 }
+
+async function startProcess(response) {
+    const headrs = response.headers;
+
+    const form = await getForm();
+
+    console.log(form);
+    
+
+    
+     
+}
+
+request('https://extrareality.by/shturm_minsk')
+    .then((response) => {
+        startProcess(response);     
+    })
+    .catch((err) => {
+        console.log(err);        
+    });
+
