50cfd6409c1a437ce2e2429fb31d3d27e65de50f
Illia Maroz <morozzz.ilya@gmail.com>
2019-02-21 13:36:28 +0300

    save

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const firstResponse = {};
 
 const getForm = () => {
     fetch('https://extrareality.by/shturm_minsk', {
+        method: 'GET',
+        mode: 'no-cors',
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         }
